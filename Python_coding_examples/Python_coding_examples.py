f0e93af84e0fb9a932d914b226e337f5bc54fd44
jancy-joseph <jancyworkmail@gmail.com>
2019-02-23 01:07:46 -0800

    Initial

---
+++ Python_coding_examples/Python_coding_examples.py
@@ -0,0 +1,14 @@
+import os
+os.getcwd()
+os.chdir('C:\\Users\\jancy\\source\\repos\\Python_coding_examples')
+os.getcwd()
+passwordFile = open('SecretPasswordFile.txt')
+secretPassword = passwordFile.read()
+print('Enter your password.')
+typedPassword = input()
+if typedPassword == secretPassword:
+    print('Access granted')
+    if typedPassword == '12345':
+      print('That password is one that an idiot puts on their luggage.')
+else:
+    print('Access denied')
